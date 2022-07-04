d64836be1795b3247cf3c29491e036dc0dfe4bff
geunnseung <106180361+geunnseung@users.noreply.github.com>
2022-07-04 23:54:36 +0900

    Day 02

---
+++ Day02/ObjectOriented2.java
@@ -0,0 +1,19 @@
+
+public class ObjectOriented2 {
+
+	public static void main(String[] args) {
+		
+		//하나의 소스파일에 여러 클래스를 작성
+		//하나의 소스파일에는 하나의 public class
+		//소스파일의 이름이 public class의 이름과 일치해야함
+		//대소문자 구분
+		//하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+		
+		
+		
+	}
+
+}
+class Hello2{}
+class Hello3{}
+
